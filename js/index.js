f124e7bf8a6c2ce082a3b432904c6ead4ea5a268
dnlKaizer <danilokaizer@gmail.com>
2025-05-06 20:29:33 -0300

    add: página de autenticação de usuário
    Adicionados jquery, jquery validate e bootstrap

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// Regras para validação de formulário de login
+$(document).ready(function () {
+    $("#form").validate({
+        rules: {
+            login: { required: true },
+            senha: { required: true }
+        },
+        messages: {
+            login: { required: "Campo obrigatório" },
+            senha: { required: "Campo obrigatório" }
+        },
+        submitHandler: function () {
+            autenticar();
+        }
+    });
+});
+
+
+async function autenticar() {
+    if ($("#form").valid()) {
+        const login = $("#login").val();
+        const senha = $("#senha").val();
+
+        try {
+            const resposta = await fetch(`https://api-odinline.odiloncorrea.com/usuario/${login}/${senha}/autenticar`);
+            const usuario = await resposta.json();
+
+            if (usuario.id > 0) {
+                localStorage.setItem("user", JSON.stringify(usuario));
+                window.location.href = "menu.html";
+            } else {
+                alert("Usuário ou senha incorretos.")
+            }
+        } catch (error) {
+            alert("Erro ao autenticar usuário.")
+        }
+    }
+}
